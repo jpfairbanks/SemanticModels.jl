014b640b108a0110251d8d353e2e11926035737d
Scott Appling <scott.appling@gtri.gatech.edu>
2019-01-25 15:08:05 -0500

    - updates for extracting function names and parameters - updates for determining which julia comments contain lexical tokens like function names and parameters names - more work can happen to continue to improve the heuristics involved with match checking - tested with NHosts1Vector.jl from https://github.com/jpfairbanks/epicookbook/blob/master/updatedJulia/HostVectorModels/NHosts1Vector.jl

--- info_extraction/src/edu/gatech/gtri/info_extraction/utils.py
+++ info_extraction/src/edu/gatech/gtri/info_extraction/utils.py
@@ -1,9 +1,9 @@
-import re
+from collections import defaultdict
 from functools import reduce
-
 from bs4 import BeautifulSoup
 from markdown import markdown
 from pathlib import Path
+import re
 
 
 # gist:https://gist.github.com/lorey/eb15a7f3338f959a78cc3661fbc255fe
@@ -24,8 +24,68 @@ def markdown_to_text(markdown_string):
     return text
 
 
-def files_in_path_with_ext(f_path, ext):
+"""
+Take an array of julia code lines and extract comments
+"""
 
+
+def julia_comment_extract(julia_code_lines):
+    keeps = []
+    for line in julia_code_lines:
+        processed_line = line.lstrip(" ")
+        if processed_line.startswith("#"):
+            keeps.append(line)
+
+    return keeps
+
+
+"""
+Take an array of julia code lines and extract parameters from julia function declarations
+"""
+
+
+def julia_param_extract(julia_code_lines):
+    keeps = []
+    func_match = r'function\s*(.*?)\((.*?)\)'
+    for line in julia_code_lines:
+
+        match = re.search(func_match, line)
+        if match:
+            grps = match.groups()
+            func_name = match.group(1)
+            params = match.group(2)
+            func_pair = [func_name, params.split(",")]
+            keeps.append(func_pair)
+
+    return keeps
+
+
+"""
+Associate tokens from comments with parameters
+"""
+
+
+def intersect_comments_params(comment_lines, param_pairs):
+    # :comment_lines are textual comments extract from julia code
+    # :param_lines are arrays of parameters extracted from each and every function signature along with function name
+
+    associations = defaultdict(list)
+    for param_pair in param_pairs:
+        for comment in comment_lines:
+            if param_pair[0] in comment:
+                associations[param_pair[0]].append(comment)
+            for param in param_pair[1]:
+                if len(param) == 1:
+                    param = " " + param + " " # looks for single variable mentions with space around
+                else:
+                    param = param + " " # look for variables and then a space (TODO: enhance this)
+                if param in comment:
+                    associations[param].append(comment)
+
+    return associations
+
+
+def files_in_path_with_ext(f_path, ext):
     if not Path(f_path).is_dir():
         raise Exception("{} is not a valid directory.".format(f_path))
 
@@ -38,13 +98,28 @@ def files_in_path_with_ext(f_path, ext):
 
 
 if __name__ == '__main__':
-    chapters_path = "/Users/scott/Downloads/epicookbook-master/_chapters"
-    out_path = "/Users/scott/Downloads/epicookbook-master/_chapters/output_txt"
 
-    for path in files_in_path_with_ext(chapters_path, "md"):
-        print(path)
-        markdown_string = reduce(lambda x, y: x + y + "\n", open(path, "r").readlines(), "")
-        text = markdown_to_text(markdown_string)
-        open(out_path + "/" + path.parent.name + "__" + path.name + ".txt", "w").write(text)
+    # Change paths to your local files for now
 
+    chapters_path = "./epicookbook-master/_chapters"
+    out_path = "./epicookbook-master/_chapters/output_txt"
 
+    sample_jl_file = "./test/info_extraction/sample_cookbook_jl/NHosts1Vector.jl"
+
+    # Run extraction from Mardown
+    def extract_markdown():
+        for path in files_in_path_with_ext(chapters_path, "md"):
+            print(path)
+            markdown_string = reduce(lambda x, y: x + y + "\n", open(path, "r").readlines(), "")
+            text = markdown_to_text(markdown_string)
+            open(out_path + "/" + path.parent.name + "__" + path.name + ".txt", "w").write(text)
+
+    def extract_jl(jl_file):
+        lines = list(open(jl_file, "r").readlines())
+        comment_lines = julia_comment_extract(lines)
+        param_pairs = julia_param_extract(lines)
+        associations = intersect_comments_params(comment_lines, param_pairs)
+        print(associations)
+
+    # julia_param_extract(["function F(du,u,p,t)"])
+    extract_jl(sample_jl_file)
