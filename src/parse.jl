bf38cc237d052f6fcabee4acf4f984e7e0d8ca63
James Fairbanks <james.fairbanks@gtri.gatech.edu>
2019-01-27 11:25:28 -0500

    add docstrings use push! instead of handle.

--- src/parse.jl
+++ src/parse.jl
@@ -1,8 +1,13 @@
 module Parsers
 using Base.Meta
+import Base.push!
 
 export parsefile
 
+"""    parsefile(path)
+
+read in a julia source file and parse it. currently only works if the top level is a simple expression or module definition.
+"""
 function parsefile(path)
     s = read(path, String)
     # open(path) do fp
@@ -13,27 +18,41 @@ function parsefile(path)
     return expr
 end
 
+"""    AbstractCollector
+
+subtypes of AbstractCollector support extracting and collecting information
+from input sources.
+"""
 abstract type AbstractCollector end
 
+
+"""    FuncCollector{T} <: AbstractCollector
+
+collects function definitions and names
+"""
 struct FuncCollector{T} <: AbstractCollector
     defs::T
 end
 
-function handle(fc::FuncCollector, expr::LineNumberNode)
-    return expr, expr
+function push!(fc::FuncCollector, expr::LineNumberNode)
+    return nothing
 end
 
-function handle(fc::FuncCollector, expr::Expr)
+function push!(fc::FuncCollector, expr::Expr)
     if expr.head == :function
         push!(fc.defs, expr.args[1] => expr.args[2])
     end
 end
 
+"""    funcs(body)
+
+collect the function definitions from a module expression.
+"""
 function funcs(body)
     fs = FuncCollector([])
     for subexpr in body
         @show subexpr
-        handle(fs, subexpr)
+        push!(fs, subexpr)
     end
     return fs
 end
