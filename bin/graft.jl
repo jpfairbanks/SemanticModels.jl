1897410e04880a729a7ec2275ef490f7ad5561bf
James Fairbanks <james.fairbanks@gtri.gatech.edu>
2019-01-27 19:53:57 -0500

    first working implementation of grafting
    the monster lives!

--- bin/graft.jl
+++ bin/graft.jl
@@ -1,47 +1,26 @@
 module Graft
-
 using Cassette
 using DifferentialEquations
 
-Cassette.@context GraftCtx
+include("../examples/epicookbook/src/SEIRmodel.jl")
+seir_ode = SEIRmodel.seir_ode
 
+using SemanticModels.Parsers
+expr = parsefile("examples/epicookbook/src/ScalingModel.jl")
 
-"""    GraftCtx
-
-grafts an expression from one simulation onto another
-
-This context is useful for modifying simulations by changing out components to add features
-
-see also: [`Dubstep.LPCtx`](@ref)
-"""
-GraftCtx
-
-function Cassette.overdub(ctx::GraftCtx, f, args...)
-    fprime = get(ctx.metadata, f, f)
-    if Cassette.canrecurse(ctx, f, args...)
-        newctx = Cassette.similarcontext(ctx, metadata = ctx.metadata)
-        return Cassette.recurse(newctx, fprime, args...)
-    else
-        return Cassette.fallback(ctx, fprime, args...)
-    end
+#vital dynamics S rate expression
+vdsre = expr.args[3].args[5].args[2].args[4]
+@show popgrowth = vdsre.args[2].args[2]
+replacevar(expr, old, new) = begin
+    dump(expr)
+    expr.args[3].args[3].args[3] = new
+    return expr
 end
+popgrowth = replacevar(popgrowth, :K,:N)
+newfunc = eval(:(fpopgrowth(r,S,N) = $popgrowth))
 
-"""    replacefunc(f::Function, d::AbstractDict)
-
-run f, but replace every call to f using the mapping in d.
-"""
-function replacefunc(f::Function, d::AbstractDict)
-    ctx = GraftCtx(metadata=d)
-    return Cassette.recurse(ctx, f)
-end
-
-end #module
-
-geteps(t) = 1.1
-
-function fprime(dY,Y,p,t)
+function fprime(dY,Y,p,t, ϵ)
     #Infected per-Capita Rate
-    ϵ = geteps(t)
     β = p[1]
     #Incubation Rate
     σ = p[2]
@@ -59,29 +38,51 @@ function fprime(dY,Y,p,t)
     #Recovered Individual
     #R = Y[4]
 
-    dY[1] = μ-β*S*I-μ*S + newfunc(ϵ, S, N)
+    dY[1] = μ-β*S*I-μ*S + newfunc(ϵ, S, S+E+I)
     dY[2] = β*S*I-(σ+μ)*E
     dY[3] = σ*E - (γ+μ)*I
 end
 
 
+Cassette.@context GraftCtx
+
+
+"""    GraftCtx
+
+grafts an expression from one simulation onto another
+
+This context is useful for modifying simulations by changing out components to add features
+
+see also: [`Dubstep.LPCtx`](@ref)
+"""
+GraftCtx
+
+function Cassette.overdub(ctx::GraftCtx, f, args...)
+    if Cassette.canrecurse(ctx, f, args...)
+        newctx = Cassette.similarcontext(ctx, metadata = ctx.metadata)
+        return Cassette.recurse(newctx, f, args...)
+    else
+        return Cassette.fallback(ctx, f, args...)
+    end
+end
+
+function Cassette.overdub(ctx::GraftCtx, f::typeof(seir_ode), args...)
+    return Cassette.fallback(ctx, fprime, args..., ctx.metadata[:lambda])
+end
+
+"""    replacefunc(f::Function, d::AbstractDict)
+
+run f, but replace every call to f using the mapping in d.
+"""
+function replacefunc(f::Function, d::AbstractDict)
+    ctx = GraftCtx(metadata=d)
+    return Cassette.recurse(ctx, f)
+end
+
+end #module
 
 using Cassette
 using DifferentialEquations
-using SemanticModels.Parsers
-include("../examples/epicookbook/src/SEIRmodel.jl")
-expr = parsefile("examples/epicookbook/src/ScalingModel.jl")
-
-#vital dynamics S rate expression
-vdsre = expr.args[3].args[5].args[2].args[4]
-@show popgrowth = vdsre.args[2].args[2]
-replacevar(expr, old, new) = begin
-    dump(expr)
-    expr.args[3].args[3].args[3] = new
-    return expr
-end
-popgrowth = replacevar(popgrowth, :K,:N)
-newfunc = eval(:(fpopgrowth(r,S,N) = $popgrowth))
 
 
 function g()
@@ -91,16 +92,17 @@ function g()
     init=[0.8,0.1,0.1]
     tspan=(0.0,365.0)
 
-    seir_prob = ODEProblem(SEIRmodel.seir_ode,init,tspan,pram)
+    seir_prob = ODEProblem(Graft.seir_ode,init,tspan,pram)
 
     sol=solve(seir_prob);
 end
 
 function scalegrowth(λ=1.0)
-    ctx = Graft.GraftCtx(metadata=Dict(SEIRmodel.seir_ode=>fprime, geteps=>(t->λ)))
+    ctx = Graft.GraftCtx(metadata=Dict(:lambda=>λ))
     return Cassette.overdub(ctx, g)
 end
-
+println("S\tI\tR")
 for λ in [1.0,1.1,1.2]
-    scalegrowth(λ)
+    S,I,R = scalegrowth(λ)(365)
+    println("$S\t$I\t$R")
 end
