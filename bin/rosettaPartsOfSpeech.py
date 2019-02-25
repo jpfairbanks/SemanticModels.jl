4905b6e69ad345d68f37d6903e786039d2a61012
Kun Cao <kcao0228@gmail.com>
2019-02-25 11:11:19 -0500

    Rosette parts of speech python

---
+++ bin/rosettaPartsOfSpeech.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 25 10:12:44 2019
+
+@author: kuncao
+"""
+
+
+
+import argparse
+import json
+import os
+import sys
+
+from rosette.api import API, DocumentParameters, RosetteException
+
+
+def run(key, alt_url, filepath):
+    """ Run the example """
+    # Create an API instance
+    api = API(user_key=key, service_url=alt_url)
+
+    # Set selected API options.
+    # For more information on the functionality of these
+    # and other available options, see Rosette Features & Functions
+    # https://developer.rosette.com/features-and-functions#morphological-analysis-introduction
+
+    # api.set_option('modelType','perceptron') # Valid for Chinese and Japanese only
+
+    morphology_parts_of_speech_data = open(inputFilePath).read()
+    params = DocumentParameters()
+    params["content"] = morphology_parts_of_speech_data
+    try:
+        return api.morphology(params, api.morphology_output['PARTS_OF_SPEECH'])
+    except RosetteException as exception:
+        print(exception)
+
+
+PARSER = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                 description='Calls the ' +
+                                 os.path.splitext(os.path.basename(__file__))[0] + ' endpoint')
+PARSER.add_argument('-k', '--key', help='Rosette API Key', required=True)
+PARSER.add_argument('-u', '--url', help="Alternative API URL",
+                    default='https://api.rosette.com/rest/v1/')
+
+if __name__ == '__main__':
+    inputFilePath = sys.argv[1]
+    RESULT = run("5e6d3db318804b1c730aed6a4bf43d38", 'https://api.rosette.com/rest/v1/', inputFilePath)
+# =============================================================================
+#     print(RESULT)
+# =============================================================================
+    print(json.dumps(RESULT, indent=2, ensure_ascii=False, sort_keys=True).encode("utf8"))
