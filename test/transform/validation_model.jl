a48407c56901ae6e46663e3623118d31f911c462
Ryan <ben_ryan@wdcryanbmacx10.gallup.com>
2019-03-18 18:48:01 -0400

    Data acquisition and autoencoder model for representing Julia source code in a low(er)-dimensional space.

--- test/transform/validation_model.jl
+++ test/transform/validation_model.jl
@@ -12,11 +12,12 @@
 using DelimitedFiles
 using Flux
 using Flux: onehot, throttle, crossentropy, onehotbatch, params, shuffle
-using MLDataPattern: stratifiedobs
+using MLDataPattern: stratifiedobs, splitobs
 using Base.Iterators: partition
 using Statistics
 
 include("../../src/validation/utils.jl")
+include("../../src/parse.jl")
 
 #
 # Set up inputs for model
@@ -25,71 +26,97 @@ include("../../src/validation/utils.jl")
 # Read lines from trace_test.dat text in to arrays of characters
 # Convert to onehot matrices
 
-cd(@__DIR__)
+maxlen = 500
+dir = "~/Documents/git/julia"
+file_type = "jl"
 
-text, alphabet, N = get_data("traces.csv")
-stop = onehot('\n', alphabet);
+#
+# use parsefile function - get Expr tree object
+#     cycle through tree - pull strings of Expr objects
+
+function read_code(dir, maxlen=500, file_type="jl")
+    comments = r"\#.*\n"
+    docstring = r"\"{3}.*?\"{3}"s
+
+    all_funcs = []
+
+    for (root, dirs, files) in walkdir(dir)
+        for file in files
+            if endswith(file, "."*file_type)
+              s = Parsers.parsefile(joinpath(root, file))
+              if !isa(s, Nothing)
+                all_funcs = vcat(all_funcs, get_expr(s, true));
+              end
+            end
+        end
+    end
+
+    filter!(x->x!="",all_funcs)
+    filter!(x -> length(x)<=maxlen, all_funcs)
+    all_funcs = unique(all_funcs)
+
+    return all_funcs
+end
 
 
-# Partition into batches of subsequences to input to our model
+function get_expr(exp_tree, verbose=false)
+    leaves = []
 
-seq_len = 50
+    for arg in exp_tree.args
+        if verbose
+            println(arg)
+        end
+        if typeof(arg) == Expr
+            if arg.head != :block
+                if verbose
+                    println("Pushed!")
+                end
+                push!(leaves, string(arg))
+            else
+                if verbose
+                    println("Recursing!")
+                end
+                leaves = vcat(leaves, get_expr(arg, verbose))
+            end
+        end
+    end
 
-Xs = [collect(partition(collect(t), seq_len)) for t in text];
-Ys = readdlm("y_results.csv");
+    return leaves
+end
+
+all_funcs = read_code(dir, maxlen, file_type)
+
+alphabet, stop, N = descr_data(all_funcs)
+stop_hot = onehot(stop, alphabet);
 
 # Transform our sequences to one hot matrices, and pad sequences to equal
 # lengths.
 
-Xs = [[onehotbatch(x_1, alphabet, '\n') for x_1 in x] for x in Xs];
-Xs = [[hcat(x_1,repeat(stop,1,seq_len-size(x_1)[2])) for x_1 in x] for x in Xs];
+Xs = [[onehotbatch(x_1, alphabet, stop) for x_1 in x] for x in all_funcs];
+Xs = [hcat(x...) for x in Xs];
+Xs = [hcat(x,repeat(stop_hot,1,maxlen-size(x)[2])) for x in Xs];
 
-dataset = [(Xs[i], onehot(Ys[i], unique(Ys))) for i in 1:length(Ys)] |> shuffle;
-labels = unique(Ys)
-Ys = [onehot(y, labels) for y in Ys];
+Xs = [reshape(x, (N*maxlen)) for x in Xs]
+train, test = splitobs(Xs, 0.7)
 
+train = batches(part for part in chunk(train, 32))...)
 
-# There are 972,290 items in our data. We use a train:test split of 90:10
+auto_encoder_mod = Chain(Dense(N*maxlen, 256, leakyrelu), 
+                         Dense(256, 128), 
+                         Dense(128, 256), 
+                         Dense(256, N*maxlen, leakyrelu))
 
-Xs = reshape(Xs, length(Xs));
-Ys = reshape(Ys, length(Ys));
-
-(Xtrain, Ytrain), (Xtest, Ytest) = stratifiedobs((Xs, Ys), p=0.9);
-
-train = [(Xtrain[i], Ytrain[i]) for i in 1:length(Ytrain)];
-test = [(Xtest[i], Ytest[i]) for i in 1:length(Ytest)];
-
-
-mod = Chain(Dense(N, 5), softmax)
-
-function forward(trc)
-  if is_leaf(trc)
-    token = embedding * string(trc.value)
-    phrase, crossentropy(mod(token), sent)
-  else
-    _, sent = tree.value
-    c1, l1 = forward(tree[1])
-    c2, l2 = forward(tree[2])
-    phrase = combine(c1, c2)
-    phrase, l1 + l2 + crossentropy(sentiment(phrase), sent)
-  end
-end
-
-
-function loss(x, y)
-	l = crossentropy(model(x), y)
+function loss(x)
+	l = mse(auto_encoder_mod(x), x)
 	return l
 end
 
-accuracy(t) = mean(argmax(model(t[1])) == argmax(t[2]))
-
-testacc() = mean(accuracy(t) for t in test)
 testloss() = mean(loss(t) for t in test)
 
 opt = ADAM(0.01)
-ps = params(model)
-evalcb = () -> @show testloss(), testacc()
+ps = params(auto_encoder_mod)
+evalcb = () -> @show testloss()
 
-Flux.train!(loss, ps, train, opt, cb = throttle(evalcb, 10))
+Flux.train!(loss, ps, zip(train), opt, cb = throttle(evalcb, 10))
 
 
