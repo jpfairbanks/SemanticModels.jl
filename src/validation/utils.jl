171d18140bc986d2e3c4bce7843d7a1527395434
Ryan <ben_ryan@wdcryanbmacx10.gallup.com>
2019-03-19 07:03:42 -0400

    Additional updated utility function for autoencoder model.

--- src/validation/utils.jl
+++ src/validation/utils.jl
@@ -3,11 +3,21 @@
 # Validation utility functions
 # 
 
-function get_data(file_name)
+function get_data(file_name::String)
     text = collect.(readlines(file_name));
     alphabet = [union(unique.(text)...)..., '\n'];
     N = length(alphabet)
 
     return text, alphabet, N
+end
+
+
+function descr_data(dat::Array)
+    alphabet = [union(unique.(collect.(dat))...)...];
+    stop = Char(maximum(float.(alphabet))+1)
+    push!(alphabet, stop)
+    N = length(alphabet)
+
+    return alphabet, stop, N
 end
 
