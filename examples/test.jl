ed0f0df3ab4752965b93c45a730d5657f6518a1d
James Fairbanks <james.fairbanks@gtri.gatech.edu>
2019-03-21 21:37:39 -0400

    add test suite for examples, can we run them

---
+++ examples/test.jl
@@ -0,0 +1,13 @@
+notebooks = ["agentbased.jl",
+             "agentgraft.jl",
+             "monomial_regression.jl",
+             "multivariate_regression.jl",
+             "polynomial_regression.jl",
+             "workflow.jl",
+             "pseudo_polynomial_regression.jl",
+             "odegraft.jl",
+             ]
+for nb in notebooks
+    @info "Running example notebook" file=nb
+    include(nb)
+end
