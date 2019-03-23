f4ea7ed90332b66bf7595a451345d8f131f7e305
James Fairbanks <james.fairbanks@gtri.gatech.edu>
2019-03-23 16:48:28 -0400

    add description of different frameworks in examples

---
+++ examples/frameworks.jl
@@ -0,0 +1,9 @@
+# | Framework | Math | Input Specification  | Description |
+# |-----------|------|----------|-------------|
+# | <img width="50px" src ="https://www.mathworks.com/content/dam/mathworks/mathworks-dot-com/images/ico_membrane_128.png"><br>Matlab/Scipy</img> | x = A\b | BLAS + scripting | Sci/Eng math is all BLAS| 
+# | <img src="https://camo.githubusercontent.com/31d60f762b44d0c3ea47cc16b785e042104a6e03/68747470733a2f2f7777772e6a756c69616f70742e6f72672f696d616765732f6a756d702d6c6f676f2d776974682d746578742e737667" alt="jump"></img> | $\min_{x\perp C(x)} f(x)$ | AMPL based DSL| Optimization Problems |
+# | <img width="50px" src="https://mc-stan.org/docs/2_18/stan-users-guide/img/logo-tm.png" alt="Stan Logo">Stan</img> | $ y \sim \mathcal{N}(x \beta + \alpha, \sigma)$ | StanML | Bayesian Inference|
+# | ![TensorFlow](https://www.gstatic.com/devrel-devsite/v64b99e7856b375caf4161a35932fd71b47bddc412a7c2f9ddea443380406e780/tensorflow/images/lockup.svg) | $y\approx f(x)$ | TF.Graph| Differentiable Programming|
+# | <img width="50px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Mathematica_Logo.svg/1920px-Mathematica_Logo.svg.png"> Mathematica</img> |$p(x)=0$|Symbolic Math Expressions| Computer Algebra Systems|
+# | <img width="75px" src="http://aske.gtri.gatech.edu/docs/latest/img/semanticmodels_jl.dot.svg">SemanticModels.jl<img> | All Computable Domains| Julia Programs | $Models \subset Code$ |
+
