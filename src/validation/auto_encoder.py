b32d1e1ce3422a91a9cf77ad44fd36718ad5687e
James Fairbanks <james.fairbanks@gtri.gatech.edu>
2019-03-25 14:35:59 -0400

    add keras.optimizers.Adam to imports list

--- src/validation/auto_encoder.py
+++ src/validation/auto_encoder.py
@@ -6,6 +6,7 @@ from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
+from keras.optimizers import Adam
 
 import numpy as np
 import pandas as pd
@@ -65,7 +66,6 @@ def ae_models(maxlen, latent_dim, N, use_gpu=False):
 
 
 # funcs = pd.read_csv("/u1/all_funcs.csv").iloc[:,0]
-funcs = fdf
 seqs, tok = chars_to_indices(funcs.iloc[:,0])
 N = len(np.unique(seqs))
 
