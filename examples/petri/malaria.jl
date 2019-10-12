61e6f5ab2da65abcfafccb61c89b200e15168d3f
James Fairbanks <james.fairbanks@gtri.gatech.edu>
2019-10-11 21:23:11 -0400

    fix compose and otimes for Open Petri Nets
    add Lotka Volterra examples and elementary food web examples

--- examples/petri/malaria.jl
+++ examples/petri/malaria.jl
@@ -2,10 +2,13 @@ module Malaria
 using Petri
 using ModelingToolkit
 import ModelingToolkit: Constant
-import Base: ==
+import Base: ==, ∈
+
+MAX_STATES = 20
+X = @variables(X[1:MAX_STATES])[1]
+STATELOOKUP = [(s,i) for (i,s) in enumerate(X)]
 
 @variables S, I, R, Sm, Im
-
 # people recover from and then lose their immunity to malaria
 rec = Petri.Model([S,I,R], [(I,R), (R, S)])
 # mosquitos recover from malaria, but are not immune
@@ -19,9 +22,6 @@ struct OpenModel{V,M}
     model::M
     codom::V
 end
-function ==(x::Petri.Model,y::Petri.Model)
-    all(isequal.(x.S, y.S)) && all(isequal.(x.Δ, y.Δ))
-end
 
 function ==(f::OpenModel,g::OpenModel)
     all(isequal.(f.dom, g.dom)) && all(isequal.(f.codom, g.codom)) && f.model == g.model
@@ -29,13 +29,46 @@ end
 
 
 ⊕(v::Vector, w::Vector) = vcat(v,w)
+# ⊕(v::Vector{Int}, w::Vector{Int}) = vcat(v,w.+length(v))
 
-function otimes(f::OpenModel{T,M}, g::OpenModel{T,M}) where {T<: Vector, M<: Petri.Model}
-    @show f.model.S
-    @show g.model.S
+function otimes(f::OpenModel{T,Md}, g::OpenModel{T,Md}) where {T<: Vector, Md<: Petri.Model}
+    f.model.S
+    g.model.S
     # TODO: need to renumber the states of g
     M = Petri.Model(f.model.S ⊕ g.model.S, f.model.Δ ⊕ g.model.Δ)
     return OpenModel(f.dom ⊕ g.dom, M, f.codom ⊕ g.codom)
+end
+
+function otimes(f::OpenModel{T,Md}, g::OpenModel{T,Md}) where {T<: Vector{Int}, Md<: Petri.Model}
+    f.model.S
+    g.model.S
+    nf = length(f.model.S)
+    ng = length(g.model.S)
+    newstates = Dict(X[s]=>X[s+nf] for (i, s) in enumerate(g.model.S))
+    replace(t::Tuple{Operation, Operation}) = (replace(t[1]), replace(t[2]))
+    replace(c::Constant) = c
+    replace(op::Operation) = begin
+        if op.op == (+)
+            return sum(map(replace, op.args))
+        end
+        if op.op == (*)
+            return prod(map(replace, op.args))
+        end
+        if length( op.args ) == 0
+            return newstates[op]
+        end
+        return op
+    end
+    newtransitions = f.model.Δ
+    if length(g.model.Δ) > 0
+        newtransitions = newtransitions ⊕ map(g.model.Δ) do t
+            replace(t)
+        end
+    end
+
+    newstatespace = collect(1:(nf+ng))
+    M = Petri.Model(newstatespace, newtransitions)
+    return OpenModel(f.dom ⊕ (g.dom .+ nf), M, f.codom ⊕ (g.codom .+ nf))
 end
 
 function equnion(a::Vector, b::Vector)
@@ -48,24 +81,60 @@ function equnion(a::Vector, b::Vector)
     return x
 end
 
+∈(x::Operation, S::Vector{Operation}) = any(isequal.(x,S))
+
 function compose(f::OpenModel{T,Md}, g::OpenModel{T,Md}) where {T<: Vector, Md<: Petri.Model}
-    X = f.dom
     Y = f.codom
     Y′ = g.dom
-    @show Y, Y′
-    @assert isequal.(Y, Y′) |> all
+    @assert length(Y) == length(Y′)
     Z = g.codom
     M = f.model
     N = g.model
 
-    states = equnion(M.S, N.S)
-    Δ = equnion(M.Δ, N.Δ)
-    Λ = equnion(M.Λ, N.Λ)
-    Φ = equnion(M.Φ, N.Φ)
+    states = vcat(M.S, ( 1:length(filter(s->!(s ∈ Y′), N.S)) ) .+ length(M.S))
+    newstates = Dict(X[Y′[i]]=>X[Y[i]] for i in 1:length(Y))
+    i = 0
+    newstates′ = map(N.S) do s
+        if s ∈ Y′
+            return nothing
+        end
+        i+=1
+        X[s] => X[i+length(M.S)]
+    end |> l-> filter(x-> x != nothing, l) |> Dict
+    newstates = union(newstates, newstates′) |> Dict
+
+    replace(t::Tuple{Operation, Operation}) = (replace(t[1]), replace(t[2]))
+    replace(c::Constant) = c
+    replace(op::Operation) = begin
+        if op.op == (+)
+            return sum(map(replace, op.args))
+        end
+        if op.op == (*)
+            return prod(map(replace, op.args))
+        end
+        if length( op.args ) == 0
+            # op ∈ keys(newstates), but for Operations
+            if any(isequal.(keys(newstates), op))
+                return newstates[op]
+            end
+            return op
+        end
+        return op
+    end
+    newtransitions = f.model.Δ
+    if length(g.model.Δ) > 0
+        newtransitions = newtransitions ⊕ map(g.model.Δ) do t
+            replace(t)
+        end
+    end
+    Δ = newtransitions
+    Λ = vcat(M.Λ, N.Λ)
+    Φ = vcat(M.Φ, N.Φ)
     Mp_yN = Petri.Model(states, Δ, Λ, Φ)
-    return OpenModel(X, Mp_yN, Z)
-    # TODO: need to renumber the states of g
-    # return Petri.Model(modulo(M.S⊕N.S, Y), f.model.Δ ⊕ g.model.Δ)
+    Z′ = map(Z) do z
+        findfirst(x->isequal(x, newstates[X[z]]), X)
+    end
+    return OpenModel(f.dom, Mp_yN, Z′)
 end
 
 f = OpenModel([S,I,R], rec, [S,I])
@@ -107,12 +176,112 @@ h2 = OpenModel([I,Im], infectm, [I])
 # # eg. Wolves hunt rabbits and sheep
 # # twoprey(k,n) = lotka(k) ⊚ onsecond ⊚ OpenModel([2], pred(n), [1,2])
 # twoprey(k,n) = (lotka(k) ⊗ Id(1)) ⊚ (Id(1) ⊗ (σ2 ⊚ pred(n) ⊚ σ2))
+
+X = @variables(X[1:20])[1]
+println("\nSpontaneous reaction spontaneous = X₁→X₂")
+spontaneous = OpenModel([1,2], Model([1,2], [(X[1],X[2])]), [1,2])
+println("\nParallel reaction parallel = spontaneous ⊗ spontaneous = X₁→X₂, X₃→X₄")
+parallel = otimes(spontaneous, spontaneous)
+println("\nInfection reaction infect = X₁+X₂→ 2X₂")
+infect = OpenModel([1,2], Model([1,2], [(X[1]+X[2], 2*X[2])]), [1,2])
+println("\nParallel Infections reactions infect ⊗ infect = X₁+X₂→ 2X₂ && X₃ +X₄ → 2X₄")
+parinfect = otimes(infect,infect)
+sponinf = compose(spontaneous, infect)
+
+NullModel(n::Int) = Model(collect(1:n), Vector{Tuple{Operation,Operation}}())
+eye(n::Int) = foldr(otimes, [OpenModel([1], NullModel(1), [1]) for i in 1:n])
+
+println("\nTesting the compose and otimes with parallel ⊚ (infect ⊗ I₂)")
+m1 = compose(parallel, otimes(infect,eye(2)))
+@show m1.dom
+@show m1.model.S
+@show m1.codom
+@show m1.model.Δ
+
+
+println("\nTesting parallel ⊚ (I₁ ⊗ infect ⊗ I₂)")
+m2 = compose(parallel, otimes(otimes(eye(1),infect),eye(1)))
+@show m2.dom
+@show m2.model.S
+@show m2.codom
+@show m2.model.Δ
+
+println("\nTesting parallel ⊚ (I₂ ⊗ infect)")
+m3 = compose(parallel, otimes(eye(2), infect))
+@show m3.dom
+@show m3.model.S
+@show m3.codom
+@show m3.model.Δ
+
+
+println("\nCreating food web processes birth, death, predation")
+birth   = Model([1], [(X[1], 2X[1])])
+death   = Model([1, 2], [(X[1], X[2])])
+pred(α,β,γ) = Model([1, 2], [(α*X[1] + β*X[2], γ*X[2])])
+
+b = OpenModel([1], birth, [1])
+d = OpenModel([1], death, [1])
+p(α, β, γ) = OpenModel([1,2], pred(α, β, γ), [1,2])
+println("\nCreating food web processes σ, predation†")
+σ() = OpenModel([2,1], NullModel(2), [2,1])
+pdag(α,β,γ) = OpenModel([1,2], Model([1, 2], [(α*X[2] + β*X[1], γ*X[1])]), [1,2])
+
+
+println("\nbd = b⊗d")
+bd = otimes(b,d)
+@show bd.model.Δ
+println("\nlv2 = bd⊚p")
+lv2 = compose(bd, p(1,2, 3))
+@show lv2.dom
+@show lv2.model.S
+@show lv2.codom
+@show lv2.model.Δ
+
+# the first predator is the second prey
+# foodchain = compose(compose(otimes(compose(otimes(b,
+#                                                   eye(1)),
+#                                            p(1,2,3)),
+#                                    eye(1)),
+#                             otimes(eye(1),
+#                                    p(1,2,3))),
+#                     otimes(otimes(eye(1), d), b))
+println("\nbdd = b⊗d⊗d")
+bdd = otimes(otimes(b, d), d)
+println("\nbdb = b⊗d⊗b")
+bdb = otimes(b,otimes(d,b))
+# foodchain = compose(compose(otimes(p(1,2,3),
+#                                    eye(1)),
+#                             otimes(eye(1),
+#                                    p(1,2,3))),
+#                     bdd)
+println("bipredation is (p⊗I)⊚(I⊗p)")
+bipredation = compose(otimes(p(1,2,3),
+                             eye(1)),
+                      otimes(eye(1),
+                             p(1,2,3)))
+# t1 = otimes(otimes(eye(1), d), eye(1))
+println("\nfoodchain is (bipredation)⊚(bdb). A fish, a bigger fish, and biggest fish")
+foodchain = compose(bipredation, bdd)
+@show foodchain.dom
+@show foodchain.model.S
+@show foodchain.codom
+@show foodchain.model.Δ
+
+# # the first predator is the second predator (with two independent prey species)
+println("\npp† is (p⊗I)⊚(I⊗p†)")
+ppdag = compose(otimes(p(1,2,3), eye(1)), otimes(eye(1), pdag(1,2,3)))
+println("\nfoodstar is (pp†)⊚(bdb). Two prey with a common predator")
+foodstar = compose(ppdag, bdb)
+@show foodstar.dom
+@show foodstar.model.S
+@show foodstar.codom
+@show foodstar.model.Δ
 end
 
 
-fog = Malaria.otimes(Malaria.f , Malaria.g)
-h   = Malaria.compose(Malaria.h1 , Malaria.h2)
-mal = Malaria.compose(fog , h)
+# fog = Malaria.otimes(Malaria.f , Malaria.g)
+# h   = Malaria.compose(Malaria.h1 , Malaria.h2)
+# mal = Malaria.compose(fog , h)
 
-mal′ = Malaria.compose(Malaria.compose(Malaria.otimes(Malaria.f,Malaria.g), Malaria.h1), Malaria.h2)
-mal′ == mal
+# mal′ = Malaria.compose(Malaria.compose(Malaria.otimes(Malaria.f,Malaria.g), Malaria.h1), Malaria.h2)
+# mal′ == mal
